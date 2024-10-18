2a7ab5df1015b6cd603ba4504968f7dbf9f6cf16
betulgozlukoglu <gozlukoglubetul@gmail.com>
2024-10-18 18:24:31 +0300

    xml dosyası ve ayarları yapıldı

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -3,19 +3,27 @@ package Utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
 public class BaseDriver {
     public static WebDriver driver;
     public static WebDriverWait wait;
-
+    @Parameters("browserTipi")
     @BeforeClass
-    public void initialOperations(){
-
+    public void initialOperations(String browserTipi){
+        switch (browserTipi.toLowerCase())
+        {
+            case "firefox":driver=new FirefoxDriver();break;
+            case "edge":driver=new EdgeDriver();break;
+            default: driver=new ChromeDriver();
+        }
         driver=new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
